c37e67962767346534388b63b2bdac02f2aa54f0
Loan <loanbillonnet@gmail.com>
2024-07-27 18:14:02 +0200

    feat: :label: return custom expired token type

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { TokenExpiredError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<User>(err: Error, user: User, info: Error) {
+    if (err || !user) {
+      if (info instanceof TokenExpiredError) {
+        throw new UnauthorizedException('Token expired');
+      }
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}
