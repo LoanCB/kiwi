354a95686673d592c04d807260820138cab0b1bf
Loan <loanbillonnet@gmail.com>
2024-07-29 19:41:33 +0200

    refactor: :card_file_box: unique title on keyword & category

--- src/orm/migrations/1722240597080-uniqueTitles.ts
+++ src/orm/migrations/1722240597080-uniqueTitles.ts
@@ -1,9 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UniqueKeywordTitle1722190504791 implements MigrationInterface {
-  name = 'UniqueKeywordTitle1722190504791';
+export class UniqueTitles1722240597080 implements MigrationInterface {
+  name = 'UniqueTitles1722240597080';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE \`category\`
+            ADD UNIQUE INDEX \`IDX_9f16dbbf263b0af0f03637fa7b\` (\`title\`)
+        `);
     await queryRunner.query(`
             ALTER TABLE \`keyword\`
             ADD UNIQUE INDEX \`IDX_a1af8669df11217cf8d9789d41\` (\`title\`)
@@ -13,6 +17,9 @@ export class UniqueKeywordTitle1722190504791 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE \`keyword\` DROP INDEX \`IDX_a1af8669df11217cf8d9789d41\`
+        `);
+    await queryRunner.query(`
+            ALTER TABLE \`category\` DROP INDEX \`IDX_9f16dbbf263b0af0f03637fa7b\`
         `);
   }
 }
